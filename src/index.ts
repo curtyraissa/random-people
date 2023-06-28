96bf9a4a08cb5d2fc538651b509a593831a05866
curtyraissa <raissacurty@gmail.com>
2023-06-29 00:08:30 +0100

    elaboracao index ts

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import indexRouter from "routes/routes";
+
+const app = express();
+app.use(express.json());
+app.use(indexRouter);
+
+const port = 5000;
+app.listen(port, () => {
+    console.log(`Server is up and running or port: ${port}`);
+})
